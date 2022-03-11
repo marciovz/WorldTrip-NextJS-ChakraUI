d9154ac6c56913447bdec6a896c49437aa4e0922
marciovz <marciovz@yahoo.com.br>
2022-03-11 12:14:11 -0300

    implementado o botão voltar para home no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,37 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { Flex, Image, Icon } from '@chakra-ui/react';
+import Link from 'next/link';
+
+import { FiChevronLeft } from "react-icons/fi";
 
 export default function Header() {
+  const { asPath } = useRouter();
+  const isHomePage = asPath === '/';
+
   return (
     <Flex 
       as='header'
-      w='100%' 
-      h={[50,100]} 
-      align='center'
+      width={['90%', '540px', '720px', '960px', '1140px']}
+      h={['50px', '60px', '75px', '90px', '100px']}
+      alignSelf='center'
+      alignItems='center'
     >
+      { !isHomePage && (
+        <Link href='/' >
+          <Icon 
+            as={FiChevronLeft}
+            width={['16px', '18px', '22px', '28px', '32px']}
+            height={['16px', '18px', '22px', '28px', '32px']}
+            color={'green.900'}
+          />
+        </Link> 
+      )}
+    
       <Image
         src='/images/Logo.svg' 
         alt='logo' 
         m='0 auto' 
-        h={['20px', '46px']}
+        h={['20px', '26px', '32px', '40px', '46px']}
       />
     </Flex>
   )
